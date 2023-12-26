da46f7ca6699468c3c0758ecb755c768254c6c61
sanathbhimsen <sanathbhimsen26@gmail.com>
2023-12-26 06:46:10 -0500

    connected the user microservice to a mongodb database(each microservice has its own db)

---
+++ MicroRetailStack/microservices/userService/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/UserServiceDB', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- MicroRetailStack/microservices/userService/index.js
+++ MicroRetailStack/microservices/userService/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const connectDB = require('./database'); // Import the connectDB function
 
 dotenv.config(); // Load environment variables
+
+// Connect to MongoDB
+connectDB();
+
 const app = express();
 
 app.use(morgan('dev')); // Logging HTTP requests
@@ -15,6 +21,20 @@ app.use('/api/users', userRoutes);
 // Basic route for testing
 app.get('/', (req, res) => {
   res.send('User Service is up and running! Lets see what happens');
+});
+
+// MongoDB connection check route
+app.get('/mongodb-status', (req, res) => {
+  const status = mongoose.connection.readyState;
+  let statusMessage = '';
+  switch (status) {
+    case 0: statusMessage = 'MongoDB Disconnected'; break;
+    case 1: statusMessage = 'MongoDB Connected'; break;
+    case 2: statusMessage = 'MongoDB Connecting'; break;
+    case 3: statusMessage = 'MongoDB Disconnecting'; break;
+    default: statusMessage = 'Unknown MongoDB Status'; break;
+  }
+  res.send(`Database Status: ${statusMessage}`);
 });
 
 const PORT = process.env.PORT || 3000;
