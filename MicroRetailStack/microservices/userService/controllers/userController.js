afdda05adc62b225639268a7030f50424b0dd42d
sanathbhimsen <sanathbhimsen26@gmail.com>
2023-12-26 07:15:00 -0500

    added instructions for mongodb

--- MicroRetailStack/microservices/userService/controllers/userController.js
+++ MicroRetailStack/microservices/userService/controllers/userController.js
@@ -1,8 +1,55 @@
-exports.register = (req, res) => {
-    // Registration logic goes here
-  };
-  
-  exports.login = (req, res) => {
-    // Login logic goes here
-  };
-  
+const bcrypt = require('bcryptjs');
+const User = require('../models/User'); // Adjust the path as necessary
+
+// User registration
+exports.register = async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        // Check if user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        // Hash the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Create a new user
+        const newUser = new User({
+            username,
+            email,
+            passwordHash: hashedPassword
+        });
+
+        // Save the user
+        await newUser.save();
+
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+// User login
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        // Find user by email
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'User not found' });
+        }
+
+        // Check if password is correct
+        const isMatch = await bcrypt.compare(password, user.passwordHash);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        res.status(200).json({ message: 'User logged in successfully' });
+        // Implement JWT token creation and return it in response for real implementation
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
---
+++ MicroRetailStack/microservices/userService/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  // Add additional fields as needed
+});
+
+module.exports = mongoose.model('User', userSchema);
